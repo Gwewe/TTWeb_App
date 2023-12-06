44d43c741b0da06512256e2e376255cda3083a01
Wedna Guirand <wedna.guirand@gmail.com>
2023-12-06 19:41:17 +0000

    edit nav bar, add change accessibility, mobileDev

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Menu function for navigation menu
+var toggle = document.querySelector('#toggle');
+var menu = document.querySelector('#myMenu');
+var menuItems = document.querySelectorAll('#myMenu li a');
+
+toggle.addEventListener('click', function(){
+    if (menu.classList.contains("is-active")) {
+        this.setAttribute('aria-expanded', 'false');
+        menu.classList.remove('is-active');
+    }else {
+        menu.classList.add("is-active");
+        this.setAttribute('aria-expanded', 'true');
+    }
+});
+
+/* document.addEventListener("DOMContentLoaded", function() {
     const largeText = document.getElementById("largeText");
     const myStart_button = document.getElementById("start_button"); // get the ID for my start button.
 
@@ -7,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Then event listener add for when user click on button
     myStart_button.addEventListener("click", function() {
+        myStart_button.style.display = "none";
         updateLargeText();
         myStart_button.classList.add("clicked!"); // add a class to trigger the button animation.
         largeText.classList.add("appear");        // Add a class to trigger the text appear animation.
@@ -18,3 +34,4 @@ document.addEventListener("DOMContentLoaded", function() {
         largeText.style.display = "block";
     };
 });
+ */
